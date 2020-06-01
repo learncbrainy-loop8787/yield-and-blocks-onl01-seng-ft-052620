b01347e8f4738fda34e640151fd5702d028ec706
korey ko <koreykendrick1@gmail.com>
2020-06-01 13:20:27 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,13 @@
 def hello_t(names)
+  if block_given?
   i = 0
 while i < names.length
 yield(names[i])
 i = i + 1
 end
 names
-end
-# call your method here!
-else 
-  puts "Hey! No block was given!"
 
-end
+
+# call your method here!
+
 
